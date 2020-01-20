f6a6e5876b2ae0a692dfa0e2920c383bad2c6aee
Rutvik Parkhiya <17.comp.rutvik.parkhiya@gmail.com>
2020-01-20 13:45:25 +0530

    practical3

---
+++ dotnetpracticals/Practical3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Please Enter Your Name");
+            name = Console.ReadLine();
+            string country;
+            Console.WriteLine("Please Enter Your Country");
+            country = Console.ReadLine();
+
+            Console.WriteLine("Helloo "+name+" From Country "+country);
+            Console.Read();
+        }
+    }
+}
