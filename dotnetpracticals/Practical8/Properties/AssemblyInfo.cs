509ec85408595b7033e35e4166e72f855b8c9120
Rutvik Parkhiya <17.comp.rutvik.parkhiya@gmail.com>
2020-05-31 20:58:55 +0530

    finalcommitupto12

--- dotnetpracticals/Practical8/Properties/AssemblyInfo.cs
+++ dotnetpracticals/Practical8/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InfixToPostfix")]
+[assembly: AssemblyTitle("Practical8")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InfixToPostfix")]
+[assembly: AssemblyProduct("Practical8")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fec175e8-781e-42a2-845c-5e0aa8f9816c")]
+[assembly: Guid("1ed6dee3-7b45-46d9-bf1c-6b276f869771")]
 
 // Version information for an assembly consists of the following four values:
 //
